2cd160b4879c5fa979c61a43862d132a986f5b7e
johnny19436 <69303376+johnny19436@users.noreply.github.com>
2020-12-19 15:48:07 +0800

    1

--- Tools/RSI_cross_test.py
+++ Tools/RSI_cross_test.py
@@ -8,7 +8,8 @@ df=pd.read_csv('C:/Users/johnn/OneDrive/桌面/repo/Data/Raw/c_Nasdaq.csv')
 
 N=9
 
-for j in range(35,100,5):
+for j in range(10,100,5):
+    print("-----"+str(j)+"-----")
     for k in range(90,j-10,-5):
         start=0
         end=0
@@ -19,12 +20,12 @@ for j in range(35,100,5):
             end+=df.iloc[i]['Close']
         end/=N
 
+        first=0
         total=0
         p=0
         for i in range(0,rsi.shape[0]):
             if rsi.iloc[i]['RSI']<=j:
-                total=df.iloc[i]['Close']
-                start=total
+                first=df.iloc[i]['Close']
                 p=1
                 break
         for i in range(9,df.shape[0]):
@@ -37,5 +38,4 @@ for j in range(35,100,5):
         if p==1:
             total+=df.iloc[df.shape[0]-1]['Close']
 
-        print(total/end)
-    print("--------"+str(j)+"---------")
+        print(round((total*start)/(first*end),2))
--- Tools/RSI_test.py
+++ Tools/RSI_test.py
@@ -6,37 +6,30 @@ df=pd.read_csv('C:/Users/johnn/OneDrive/桌面/repo/Data/Raw/c_DJI.csv')
 N=9
 
 start=0
+end=0
 for i in range(0,N):
     start+=df.iloc[i]['Close']
 start/=N
-
-end=0
 for i in range(df.shape[0]-N,df.shape[0]):
     end+=df.iloc[i]['Close']
 end/=N
 
+first=0
 total=0
 p=0
-
 for i in range(0,rsi.shape[0]):
-    if rsi.iloc[i]['RSI']<=90:
-        total=df.iloc[i]['Close']
-        start=total
+    if rsi.iloc[i]['RSI']<=20:
+        first=df.iloc[i]['Close']
         p=1
         break
-
-for i in range(0,rsi.shape[0]):
-    if p==0 and rsi.iloc[i]['RSI']<=90:
-        total-=df.iloc[i+N]['Close']
+for i in range(9,df.shape[0]):
+    if p==0 and rsi.iloc[i-N]['RSI']<=20:
+        total-=df.iloc[i]['Close']
         p=1
-    elif p==1 and rsi.iloc[i]['RSI']>=90:
-        total+=df.iloc[i+N]['Close']
+    elif p==1 and rsi.iloc[i-N]['RSI']>=80:
+        total+=df.iloc[i]['Close']
         p=0
 if p==1:
     total+=df.iloc[df.shape[0]-1]['Close']
-print(total/start)
 
-print(end/start)
-print(total/end)
-#print(total/end)
-
+print(round((total*start)/(first*end),2))
